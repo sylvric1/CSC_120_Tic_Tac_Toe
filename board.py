73d57894a3f044664eae5ca8da9073fbcaf9b870
Kevin Richard <kevin.rohit@gmail.com>
2022-07-13 15:00:30 -0400

    Board Printing functionality added

---
+++ board.py
@@ -0,0 +1,8 @@
+rowOne = [" _ ", " _ ", " _ "]
+rowTwo = [" _ ", " _ ", " _ "]
+rowThree = [" _ ", " _ ", " _ "]
+gameBoard = [rowOne, rowTwo, rowThree]
+index = 0
+while index < 3:
+    print(gameBoard[index])
+    index = index + 1

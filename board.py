d29050ebc660016f49de1a57d91f6b1186df9a9a
Kevin Richard <kevin.rohit@gmail.com>
2022-07-14 13:38:24 -0400

    Stage 3: Game Logic added to show results

--- board.py
+++ board.py
@@ -49,21 +49,43 @@ def placeMark(row, column, player):
 
 
 def gameOver(player):
-    gameResult = ""
-    if rowOne == [player, player, player] or rowTwo == [player, player, player] or rowThree == [player, player, player]:
-        gameResult = "Game Over"
-    elif rowOne[0] == player and rowTwo[0] == player and rowThree[0] == player:
-        gameResult = "Game Over"
-    elif rowOne[1] == player and rowTwo[1] == player and rowThree[1] == player:
-        gameResult = "Game Over"
-    elif rowOne[2] == player and rowTwo[2] == player and rowThree[2] == player:
-        gameResult = "Game Over"
-    elif rowOne[0] == player and rowTwo[1] == player and rowThree[2] == player:
-        gameResult = "Game Over"
-    elif rowOne[2] == player and rowTwo[1] == player and rowThree[0] == player:
-        gameResult = "Game Over"
+    playerName = ""
+    if player == " X ":
+        playerName = 1
     else:
-        gameResult = ""
+        playerName = 2
+    if rowOne == [player, player, player] or rowTwo == [player, player, player] or rowThree == [player, player, player]:
+        gameResult = "Game Over. Player " + str(playerName) + " wins!"
+    elif rowOne[0] == player and rowTwo[0] == player and rowThree[0] == player:
+        gameResult = "Game Over. Player " + str(playerName) + " wins!"
+    elif rowOne[1] == player and rowTwo[1] == player and rowThree[1] == player:
+        gameResult = "Game Over. Player " + str(playerName) + " wins!"
+    elif rowOne[2] == player and rowTwo[2] == player and rowThree[2] == player:
+        gameResult = "Game Over. Player " + str(playerName) + " wins!"
+    elif rowOne[0] == player and rowTwo[1] == player and rowThree[2] == player:
+        gameResult = "Game Over. Player " + str(playerName) + " wins!"
+    elif rowOne[2] == player and rowTwo[1] == player and rowThree[0] == player:
+        gameResult = "Game Over. Player " + str(playerName) + " wins!"
+    else:
+        rowOneFilled = 0
+        rowTwoFilled = 0
+        rowThreeFilled = 0
+        index = 0
+        while index < 3:
+            if index == 0:
+              if rowOne[0] != " _ " and rowOne[1] != " _ " and rowOne[2] != " _ ":
+                  rowOneFilled = 1
+            elif index == 1:
+                if rowTwo[0] != " _ " and rowTwo[1] != " _ " and rowTwo[2] != " _ ":
+                  rowTwoFilled = 1
+            elif index ==2:
+                if rowThree[0] != " _ " and rowThree[1] != " _ " and rowThree[2] != " _ ":
+                  rowThreeFilled = 1
+            index = index + 1
+        if rowOneFilled == 1 and rowTwoFilled == 1 and rowThreeFilled == 1:
+            gameResult = "Game Over. Draw."
+        else:
+            gameResult = ""
     return gameResult
 
 
@@ -80,15 +102,20 @@ while gameResult == "":
         squareCheck = checkMark(row, column)
     placeMark(row, column, player)
     gameResult = gameOver(player)
-    printBoard()
-    squareCheck = "False"
-    print("Player 2's turn.")
-    player = " O "
-    while squareCheck == "False":
-        print("Enter a row number from 0-2:")
-        row=input()
-        print("Enter a column number from 0-2")
-        column=input()
-        squareCheck = checkMark(row, column)
-    placeMark(row, column, player)
-    gameResult = gameOver(player)
+    if gameResult != "":
+        break
+    else:
+        printBoard()
+        squareCheck = "False"
+        print("Player 2's turn.")
+        player = " O "
+        while squareCheck == "False":
+            print("Enter a row number from 0-2:")
+            row=input()
+            print("Enter a column number from 0-2")
+            column=input()
+            squareCheck = checkMark(row, column)
+        placeMark(row, column, player)
+        gameResult = gameOver(player)
+
+print(gameResult)
